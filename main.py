79c5d76d9d9e9477d57994a692508121d0d3a6e8
Tyler-Shepherd <tyleremail7@gmail.com>
2022-05-02 23:34:30 -0700

    is possible

--- main.py
+++ main.py
@@ -72,63 +72,73 @@ def random_solve(target, words):
     return num_iterations
 
 
+def is_possible(possible_word, colors, guess):
+    used = [False] * len(possible_word)
+
+    for (pos, color) in enumerate(colors):
+        guessed_letter = guess[pos]
+
+        # all greens in same place
+        if color == Color.GREEN:
+            if possible_word[pos] != guessed_letter:
+                return False
+            used[pos] = True
+
+        if color == Color.YELLOW:
+            # yellows not in the same place
+            if possible_word[pos] == guessed_letter:
+                return False
+
+            # every yellow reused
+            reused_yellow = next(
+                (x for x in range(len(possible_word)) if possible_word[x] == guessed_letter and not used[x]), None)
+            if reused_yellow is None:
+                return False
+            used[reused_yellow] = True
+
+        # no grays used
+        if color == Color.GRAY:
+            used_gray = next(
+                (x for x in range(len(possible_word)) if possible_word[x] == guessed_letter and not used[x]), None)
+            if used_gray is not None:
+                return False
+
+        # test cases:
+        # target is "lefty". first guess is "forte". says the 'e' is yellow
+        # it should not try "lefte". even though there is an e in different place, the last e is in same
+
+        # if you guess something with 2 'e's, and both are yellow
+        # then every guess after has to have at least 2 'e's
+
+        # if you guess 3 'e's and 2 are yellow
+        # every guess after has to have exactly 2 'e's
+
+    return True
+
+
 def random_hard_solve(target, words):
-    num_iterations = 0
+    num_iterations = 1
     guessable_words = set(words)
-    known_greens = set()
-    known_yellows = set()
-    known_grays = set()
 
     while num_iterations < 1000:
         guess = random.choice(list(guessable_words))
         colors = colorize(target, guess)
-        print(num_iterations, guess, print_colors(colors))
 
-        if all_green(colors):
-            break
 
         guessable_words.remove(guess)
 
-        for c, color in enumerate(colors):
-            guessed_letter = guess[c]
-            if color == Color.GREEN:
-                known_greens.add((c, guessed_letter))
-            elif color == Color.YELLOW:
-                known_yellows.add((c, guessed_letter))
-            else:
-                known_grays.add(guessed_letter)
-
+        # go through guessable_words and remove all those that dont fit colors
         for possible_word in list(guessable_words):
-            should_remove = False
-
-            # all greens in same place
-            for (pos, letter) in known_greens:
-                if possible_word[pos] != letter:
-                    should_remove = True
-
-            # all yellow letters in different places
-            for (pos, letter) in known_yellows:
-                yellow_letter_pos = possible_word.find(letter)
-                if yellow_letter_pos == -1 or yellow_letter_pos == pos:
-                    should_remove = True
-
-                # this doesnt work for repeated letters
-                # for example: target is "lefty". first guess is "forte". says the 'e' is yellow
-                # it should not try "lefte". even though there is an e in different place, the last e is in same
-
-                # other issue: if you guess something with 2 'e's, and both are yellow
-                # then every guess after has to have at least 2 'e's
-
-                # or if you guess 3 'e's and 2 are yellow
-                # every guess after has to have exactly 2 'e's
-
-                # so we cant keep a big set on per-letter basis. has to be per-guess with context of rest of guess
-
-
-            # no gray letters
-
-            if should_remove:
+            if not is_possible(possible_word, colors, guess):
                 guessable_words.remove(possible_word)
+
+        num_remaining = len(guessable_words)
+        print(num_iterations, guess, print_colors(colors), num_remaining)
+        if all_green(colors):
+            break
+
+        if num_remaining < 10:
+            print(guessable_words)
 
         num_iterations += 1
 
@@ -141,9 +151,13 @@ if __name__ == '__main__':
     words = get_words()
 
     # all lower case for now
-    target = "lefty"
+    target = "story"
 
     iters = random_hard_solve(target, words)
-
     print(target, iters)
+
+
+    # print(is_possible("iiee", [Color.YELLOW, Color.GRAY, Color.GRAY, Color.GRAY], "eexx"))
+
+
 
